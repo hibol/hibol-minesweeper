fd06ca11ab80b8eb1b9c0841af188f2306238853
hibol <hibol18@gmail.com>
2026-07-19 19:09:31 +0200

    add githuib pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Site servi sous https://hibol.github.io/hibol-minesweeper/ (GitHub Pages
+  // de projet, pas de user/org) : doit correspondre au nom du repo.
+  base: '/hibol-minesweeper/',
   plugins: [vue()],
 })
